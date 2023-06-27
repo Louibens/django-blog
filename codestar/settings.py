01a6c019aa6102c362384b485b8b23eefcec45ec
Louise Benson <louise.benson@gmail.com>
2023-06-27 16:33:03 +0000

    fu

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-louibens-djangoblog-k5u0f7rzttt.ws-eu100.gitpod.io/', 'django-blog-lb.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-louibens-djangoblog-k5u0f7rzttt.ws-eu100.gitpod.io/', 
+                 'django-blog-lb.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -132,7 +133,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_stoarge.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
